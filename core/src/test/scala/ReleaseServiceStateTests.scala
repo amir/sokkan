83c84ffda902e6eeb921210f0e230ddae6e7fb1c
Amir Saeid <amir@glgdgt.com>
2018-04-12 13:09:49 +0100

    Add a test for parsing charts

--- core/src/test/scala/ReleaseServiceStateTests.scala
+++ core/src/test/scala/ReleaseServiceStateTests.scala
@@ -1,17 +1,30 @@
+import java.io.File
+
 import cats.{Id, ~>}
 import hapi.chart.chart.Chart
+import hapi.chart.config.Config
+import cats.instances.try_._
 import hapi.release.release.Release
 import hapi.services.tiller.tiller._
+import io.iteratee.modules.{IdModule, TryModule}
 import org.scalatest.{FlatSpec, Matchers}
-import sokkan.{ReleaseServiceA, SokkanOp}
+import org.scalatest.TryValues._
+import sokkan.{ReleaseServiceA, RootChart, SokkanOp}
 import sokkan.SokkanOp._
+import sokkan.iteratee.chart.ChartModule
+import sokkan.iteratee.tar.NonSuspendableTapeArchiveModule
+
+import scala.util.{Success, Try}
 
 class ReleaseServiceStateTests extends FlatSpec with Matchers {
 
-  val interpreter: ReleaseServiceA ~> Id = new (ReleaseServiceA ~> Id) {
+  val interpreter: ReleaseServiceA ~> Try = new (ReleaseServiceA ~> Try) {
     val state = scala.collection.mutable.Map.empty[String, Option[Chart]]
 
-    def apply[A](fa: ReleaseServiceA[A]): Id[A] =
+    trait TryChartModule extends TryModule with NonSuspendableTapeArchiveModule[Try] with ChartModule[Try]
+    final object tryCharts extends TryChartModule
+
+    def apply[A](fa: ReleaseServiceA[A]): Try[A] =
       fa match {
         case InstallRelease(req: InstallReleaseRequest) =>
           def in(n: String, idx: Int): Int = {
@@ -23,7 +36,7 @@ class ReleaseServiceStateTests extends FlatSpec with Matchers {
             }
           }
           val v = in(req.name, 1)
-          InstallReleaseResponse(Some(Release(name = req.name, version = v)))
+          Success(InstallReleaseResponse(Some(Release(name = req.name, version = v))))
 
         case UpdateRelease(req: UpdateReleaseRequest) =>
           val rs = state.keySet.filter(_.startsWith(s"${req.name}.v")).toList
@@ -32,10 +45,19 @@ class ReleaseServiceStateTests extends FlatSpec with Matchers {
 
           state(s"${req.name}.v${newV}") = req.chart
 
-          UpdateReleaseResponse(Some(Release(name = req.name, version = newV)))
+          Success(UpdateReleaseResponse(Some(Release(name = req.name, version = newV))))
 
         case GetReleaseContent(req: GetReleaseContentRequest) =>
-          GetReleaseContentResponse(release = state.get(s"${req.name}.v${req.version}").map(c => Release(chart = c)))
+          Success(GetReleaseContentResponse(release = state.get(s"${req.name}.v${req.version}").map(c => Release(chart = c))))
+
+        case GetChartFromTapeArchive(file: File) =>
+          tryCharts.chartFromFiles(tryCharts.readTapeArchiveStreams(file)).run.map { s =>
+            s.get(RootChart).map { rootChart =>
+              val deps = (s - RootChart).values.toList
+              rootChart.copy(dependencies = deps)
+            }
+          }
+
 
       }
   }
@@ -47,5 +69,12 @@ class ReleaseServiceStateTests extends FlatSpec with Matchers {
     u <- SokkanOp.update(UpdateReleaseRequest(name = name))
   } yield u.release
 
-  sokkan.run(interpreter, p) should matchPattern { case Some(Release(name, _, _, _, _, _, 2, _)) => }
+  sokkan.run(interpreter, p).success.value should matchPattern { case Some(Release(name, _, _, _, _, _, 2, _)) => }
+
+  val p2: ReleaseService[Option[Chart]] =
+    SokkanOp.chartFromTapeArchive(new File(getClass.getResource("mychart-0.1.0.tgz").getPath))
+
+  val chart = sokkan.run(interpreter, p2)
+
+  chart.success.value shouldBe defined
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,5 @@
 addSbtPlugin("com.thesamet" % "sbt-protoc" % "0.99.17")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
 
 libraryDependencies ++= Seq(
   "com.thesamet.scalapb" %% "compilerplugin" % "0.7.0",
